59506a7f3c64c796825d6a6222b3ec49a3fffa8b
Luca Molteni <moltek96@gmail.com>
2018-12-03 18:19:27 +0100

    Add some constraint to UI

--- iOS/Data4Help/Data4Help/Controller/MainControllers/ViewController.swift
+++ iOS/Data4Help/Data4Help/Controller/MainControllers/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let dataManager:DataManager = DataManager()
         dataManager.authorizeHKinApp()
-        dataManager.getHeartRates()
+        //dataManager.getHeartRates()
         //dataManager.enableBackgroundData()
     }
 
